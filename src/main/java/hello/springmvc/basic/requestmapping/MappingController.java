bd228a5085bdce23b32bc55692e33513c322d4ba
조인혁 <alzpqm@naver.com>
2022-01-06 19:01:10 +0900

    refactor: @RequestMapping을 @GetMapping으로 변경

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,5 +1,6 @@
 package hello.springmvc.basic.requestmapping;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class MappingController {
-	@RequestMapping(value = {"/hello-basic", "/hello-go"}, method = RequestMethod.GET)
+	@GetMapping({"/hello-basic", "/hello-go"})
 	public String helloBasic() {
 		log.info("helloBasic");
 		return "ok";
