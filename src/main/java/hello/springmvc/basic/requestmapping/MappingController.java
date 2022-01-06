25067e70d58678bf7924f77aa4ad949fb96027be
조인혁 <alzpqm@naver.com>
2022-01-06 18:59:49 +0900

    fix: helloBasic Controller가 GET method에서만 동작하도록 수정

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,6 +1,7 @@
 package hello.springmvc.basic.requestmapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class MappingController {
-	@RequestMapping({"/hello-basic", "/hello-go"})
+	@RequestMapping(value = {"/hello-basic", "/hello-go"}, method = RequestMethod.GET)
 	public String helloBasic() {
 		log.info("helloBasic");
 		return "ok";
