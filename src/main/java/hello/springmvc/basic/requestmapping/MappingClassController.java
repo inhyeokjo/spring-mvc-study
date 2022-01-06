45b4c55abbb8b6849872e71d47969c2ec43a5222
조인혁 <alzpqm@naver.com>
2022-01-06 19:39:18 +0900

    refactor: 공통 url "/users" 묶기

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -1,37 +1,38 @@
 package hello.springmvc.basic.requestmapping;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class MappingClassController {
 
-	@GetMapping("/users")
+	@GetMapping
 	public String users() {
 		return "get users";
 	}
 
-	@PostMapping("/users")
+	@PostMapping
 	public String addUser() {
 		return "add users";
 	}
 
-	@GetMapping("/users/{userId}")
+	@GetMapping("/{userId}")
 	public String findUser(@PathVariable Long userId) {
-		return "find user:"+userId;
+		return "find user:" + userId;
 	}
 
-	@PostMapping("/users/{userId}")
+	@PostMapping("/{userId}")
 	public String updateUser(@PathVariable Long userId) {
-		return "update user:"+userId;
+		return "update user:" + userId;
 	}
 
-	@DeleteMapping("/users/{userId}")
+	@DeleteMapping("/{userId}")
 	public String deleteUser(@PathVariable Long userId) {
-		return "delete user:"+userId;
+		return "delete user:" + userId;
 	}
 }
