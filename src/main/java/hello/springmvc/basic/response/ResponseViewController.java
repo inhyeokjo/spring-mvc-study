a5fc65da6f4911bdec113d4845b47ec9f5ac9dd5
조인혁 <alzpqm@naver.com>
2022-01-08 18:26:51 +0900

    feat: ViewName을 return하지 않고 url에 있는 리소스를 그대로 ViewName으로 사용하는 return void 컨트롤러 구현

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -9,15 +9,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class ResponseViewController {
 
 	@RequestMapping("/response-view-v1")
-	public ModelAndView responseViewV1(){
+	public ModelAndView responseViewV1() {
 		ModelAndView mav = new ModelAndView("response/hello")
 				.addObject("data", "hello!");
 		return mav;
 	}
 
 	@RequestMapping("/response-view-v2")
-	public String responseViewV2(Model model){
+	public String responseViewV2(Model model) {
 		model.addAttribute("data", "hello!");
 		return "response/hello";
+	}
+
+	@RequestMapping("/response/hello")
+	public void responseViewV3(Model model) {
+		model.addAttribute("data", "hello!");
 	}
 }
